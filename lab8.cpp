106c47b70aed231365507b6ee11c3634756eb6a3
Vadim Belskyi <vrb424@gmail.com>
2022-12-07 11:57:15 +0200

    return ;

--- lab8.cpp
+++ lab8.cpp
@@ -7,35 +7,35 @@ using namespace std;
 
 struct book {
 
-    char surname[30]
-    char specialist[30]
-    int year
+    char surname[30];
+        char specialist[30];
+        int year;
 
 };
 
 void show_book(book b)
 {
 
-    cout << "Прізвище лікаря: " << b.surname << endl 
-    cout << "Спеціалізація: " << b.specialist << endl  
-    cout << "Стаж роботи: " << b.year << endl
+    cout << "Прізвище лікаря: " << b.surname << endl;
+        cout << "Спеціалізація: " << b.specialist << endl;
+        cout << "Стаж роботи: " << b.year << endl;
 
 }
 
 int main()
 {
-    SetConsoleCP(1251
-    SetConsoleOutputCP(1251
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
 
-    book a = { " Іванченко С.С.","Паталагоанатом", 4
+    book a = { " Іванченко С.С.","Паталагоанатом", 4;
     show_book(a);
-    book b = { " Палагній С.О.","Гениколог", 3
+    book b = { " Палагній С.О.","Гениколог", 3;
     show_book(b);
-    book c = { " Бельський В.Р.","Хірург", 4
+    book c = { " Бельський В.Р.","Хірург", 4;
     show_book(c);
-    book d = { " Лихін І.І ","Стоматолог" ,2
+    book d = { " Лихін І.І ","Стоматолог" ,2;
     show_book(d)ж
-    cout << "\nВведіть стаж роботи лікаря" << endl
+        cout << "\nВведіть стаж роботи лікаря" << endl;
 
 }
